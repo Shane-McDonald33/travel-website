00ab67ac89e6883f69250f549fc11b7160ba159d
aydenapollo <mcdonaldshane33@gmail.com>
2023-01-01 14:00:06 -0600

    building organization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+
 
 function App() {
   return (
     <div className="App">
-      Working?
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' exact element={<Home/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
