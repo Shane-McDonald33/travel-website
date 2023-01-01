e16911c31e274f536a5a1893bab4d1e3f34b99ec
aydenapollo <mcdonaldshane33@gmail.com>
2023-01-01 17:53:57 -0600

    some more building blocks

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,27 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import ReorderIcon from '@mui/icons-material/Reorder';
-import CloseIcon from '@mui/icons-material/Close';
-
+//import CloseIcon from '@mui/icons-material/Close';
+import { Button } from './Button';
+import '../Styles/Navbar.css';
 
 function Navbar() {
     const [openLinks, setLinks] = useState(false);
+    const [button, setButton] = useState(true);
     const toggleNavBar = () => {
         setLinks(!openLinks)
-        if (openLinks) {
-            return <CloseIcon/>;
-        }
     };
     const closeMobileMenu = () => setLinks(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
   return (
     <>
     <nav className="navbar">
@@ -31,7 +40,23 @@ function Navbar() {
                     Home
                 </Link>
             </li>
+            <li className='nav-iem'>
+                <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                    Services
+                </Link>
+            </li>
+            <li className='nav-iem'>
+                <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                    Products
+                </Link>
+            </li>
+            <li className='nav-iem'>
+                <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                    Sign-Up
+                </Link>
+            </li>
         </ul>
+        {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
         </div>
     </nav>
     </>
